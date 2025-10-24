0cc874ba8080832aa9018a859984b85884c211a8
tushar7789 <tusharjangid7@gmail.com>
2025-10-24 20:44:58 +0530

    [mod] complted carousel with custom buttons & next, prev functions

--- src/app/villas/[villaId]/page.tsx
+++ src/app/villas/[villaId]/page.tsx
@@ -1,24 +1,99 @@
 'use client'
 
-import React from 'react'
+import React, { useState, createRef, RefObject } from 'react'
 import { usePathname } from 'next/navigation';
 import { Carousel } from "antd";
+import { CarouselRef } from 'antd/es/carousel';
 import Image from 'next/image';
 
 import Button from '@/_components/button';
-import LivingRoomImg from '../../../../public/static_images/living-room-i.png';
-import BedroomImg from '../../../../public/static_images/bedroom-i.png';
-import KitchenImg from '../../../../public/static_images/kitchen-i.png';
-import BathroomImg from '../../../../public/static_images/bathroom-i.png';
-import BalconyImg from '../../../../public/static_images/balcony-i.png';
+import ImageList from '@/utils/utils';
+import Arrow from '@/_components/arrows';
 
+const modeLeft: string = "left";
 
 function VillaId() {
     const pathname = usePathname();
     const id = pathname.split("/")[2];
+    const len = ImageList.length;
+
+    const [curLabelInd, setCurLabelInd] = useState<number>(0);
+
+    const carouselRef: RefObject<CarouselRef | null> = createRef<CarouselRef>();
+
+    // function handleBeforeChange() {
+    //     const len = ImageList.length;
+    //     setCurLabelInd(curLabelInd => (curLabelInd + 1) % len);
+    // }
+
+    function handlePrev() {
+        if (curLabelInd === 0) return;
+
+        setCurLabelInd(curLabelInd => (curLabelInd - 1) % len);
+        carouselRef.current?.prev();
+    }
+
+
+    function handleNext() {
+        if (curLabelInd === len - 1) return;
+
+        setCurLabelInd(curLabelInd => (curLabelInd + 1) % len);
+        carouselRef.current?.next();
+    }
+
     return (
         <>
-            <div className='flex flex-row h-[460px] width-[820px] items-center justify-between mt-[60px] mx-[50px] backdrop-brightness-30'>
+            <div className='
+                        flex
+                        flex-row
+                        h-[40px]
+                        w-screen
+                '>
+                <span className='
+                        h-[40px]
+                        w-[125px]
+                        flex
+                        items-center
+                        relative
+                        left-[1055px]
+                        top-[80px]
+                        z-20
+                        bg-[white]
+                        text-[24px]
+                        pl-[10px]
+                '>
+                    {ImageList[curLabelInd][1].toString()}
+                </span>
+                {
+                    curLabelInd !== 0 ?
+                        <span className='
+                        relative
+                        left-[675px]
+                        top-[250px]
+                        z-20
+                '
+                            onClick={handlePrev}
+                        >
+                            <Arrow mode="left" />
+                        </span> :
+                        null
+                }
+                {
+                    curLabelInd !== len - 1 ?
+                        <span className={`
+                                relative
+                                top-[250px]
+                                z-20
+                                ${curLabelInd !== 0 ? 'left-[1000px]' : 'left-[1030px]'}
+                            `}
+                            onClick={handleNext}
+                        >
+                            <Arrow mode="right" />
+                        </span> :
+                        null
+                }
+            </div >
+            <div className='flex flex-row h-[460px] width-[820px] items-center justify-between  mx-[50px] backdrop-brightness-30'>
                 <div className='
                             flex 
                             flex-col
@@ -37,46 +112,29 @@ function VillaId() {
                     <Button to="/" type="secondary">Book</Button>
                 </div>
                 <Carousel
-                    autoplay={{ dotDuration: true }}
-                    autoplaySpeed={5000}
+                    infinite={false}
                     className='
                         h-[400px] 
                         w-[400px]
                         mr-[30px]
                     '
+                    // beforeChange={handleBeforeChange}
+                    ref={carouselRef}
                 >
-                    <div>
-                        <Image
-                            src={LivingRoomImg}
-                            alt="living-room-img"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src={BedroomImg}
-                            alt="bedroom-img"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src={KitchenImg}
-                            alt="kitchen-img"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src={BathroomImg}
-                            alt="bathroom-img"
-                        />
-                    </div>
-                    <div>
-                        <Image
-                            src={BalconyImg}
-                            alt="balcony-img"
-
-                        />
-                    </div>
+                    {
+                        ImageList.map((img, ind) => {
+                            return (
+                                <div key={ind}>
+                                    <Image
+                                        src={img[0]}
+                                        alt={img[1].toString()}
+                                    />
+                                </div>
+                            );
+                        })
+                    }
                 </Carousel>
+
             </div>
         </>
     )
---
+++ src/utils/utils.tsx
@@ -0,0 +1,15 @@
+import LivingRoomImg from '../..//public/static_images/living-room-i.png';
+import BedroomImg from '../../public/static_images/bedroom-i.png';
+import KitchenImg from '../../public/static_images/kitchen-i.png';
+import BathroomImg from '../../public/static_images/bathroom-i.png';
+import BalconyImg from '../../public/static_images/balcony-i.png';
+
+const ImageList = [
+    [LivingRoomImg, "Living Room"],
+    [BedroomImg, "Bedroom"],
+    [KitchenImg, "Kitchen"],
+    [BathroomImg, "Bathroom"],
+    [BalconyImg, "Balcony"]
+];
+
+export default ImageList;
