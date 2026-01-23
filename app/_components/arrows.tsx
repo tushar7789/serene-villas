726bc09b789a932025f558976ed1aefb32b40b7f
tushar7789 <tusharjangid7@gmail.com>
2026-01-23 17:10:02 +0530

    added filter tags and applied filters comp

--- app/_components/arrows.tsx
+++ app/_components/arrows.tsx
@@ -1,22 +1,38 @@
 import Image from "next/image";
 import Left from "../../public/static_images/left-arrow.png";
 import Right from "../../public/static_images/right-arrow.png";
+import FilterTagArrow from "../../public/static_images/arrow.png"
+import FilterDirArrow from "../../public/static_images/filter-arrow.png"
 
 type modeType = {
-    mode: string
+    type?: string,
+    mode?: string,
+    dimension?: string
 };
 
-export default function Arrow({ mode }: modeType) {
+export default function Arrow({ type, mode, dimension }: modeType) {
+    // const ClassName = `${dimension !== undefined ? `h-${dimension} w-${dimension}` : "h-7 w-7"} cursor-pointer`
+    const dim = Number(`${dimension !== undefined ? dimension : "30"}`)
     return (
         <>
             {
-                mode === "left" ?
-                    <Image src={Left} alt="left" className="h-7 w-7 cursor-pointer" /> :
-                    mode === "right" ?
-                        <Image src={Right} alt="right" className="h-7 w-7 cursor-pointer" /> :
-                        mode === "up" ?
-                            <Image src={Left} alt="right" className="h-7 w-7 cursor-pointer rotate-90" /> :
-                            <Image src={Right} alt="right" className="h-7 w-7 cursor-pointer rotate-90" />
+                type === undefined ?
+                    mode === "left" ?
+                        <Image src={Left} height={dim} width={dim} alt="left" /> :
+                        mode === "right" ?
+                            <Image src={Right} height={dim} width={dim} alt="right" /> :
+                            mode === "up" ?
+                                <Image src={Left} height={dim} width={dim} alt="right" className="cursor-pointer rotate-90" /> :
+                                <Image src={Right} height={dim} width={dim} alt="right" className="cursor-pointer rotate-90" />
+                    :
+                    type === "filter-dir" ?
+                        mode === "down" ?
+                            <Image src={FilterDirArrow} height={dim} width={dim} alt="right" className="cursor-pointer rotate-180" /> :
+                            <Image src={FilterDirArrow} height={dim} width={dim} alt="right" className="cursor-pointer" />
+                        :
+                        mode === "down" ?
+                            <Image src={FilterTagArrow} height={dim} width={dim} alt="right" className="cursor-pointer rotate-90" /> :
+                            <Image src={FilterTagArrow} height={dim} width={dim} alt="right" className="cursor-pointer rotate-270" />
             }
         </>
     );
--- app/_components/filterPanel.tsx
+++ app/_components/filterPanel.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { DatePicker } from 'antd';
+import Divider from '@mui/material/Divider';
 
 import Arrow from '../_components/arrows';
 import getTodaysDate from '../_utils/functions/timeAndDate';
 import Button from './button';
 import { filteredVillaFunction, clearFilterValues } from "../_utils/functions/filterPanelFunctions";
+
+interface FilterPanelUtillityInterface {
+    children?: React.ReactNode,
+    title?: string
+}
 
 const FilterPanel = () => {
     const [from, setFrom] = useState<string | null>("");
@@ -30,34 +36,34 @@ const FilterPanel = () => {
                 <Button type="secondary" callbackSetter={handleApplyFilters}>Apply Filter</Button>
                 <Button type="secondary" callbackSetter={clearFilterValues}>Clear</Button>
             </div>
-            <div className='h-15 w-full flex justify-end items-center'>
-                <span className='h-full w-7 mr-2 flex justify-center items-center' onClick={() => setFilterDirection("unc")}>
-                    <Arrow mode='up' />
+            <div className='h-15 w-full my-2 flex justify-end items-center'>
+                <span className='h-full w-6 flex justify-center items-center' onClick={() => setFilterDirection("unc")}>
+                    <Arrow type='filter-dir' mode='up' />
                 </span>
-                <span className='h-full w-7 flex justify-center items-center' onClick={() => setFilterDirection("dec")}>
-                    <Arrow mode='down' />
+                <span className='h-full w-6 flex justify-center items-center' onClick={() => setFilterDirection("dec")}>
+                    <Arrow type="filter-dir" mode='down' />
                 </span>
             </div>
-            <div className='h-min w-full flex flex-col justify-start items-center bg-emerald-600'>
-                <div className='h-50 w-full flex flex-col justify-start items-center'>
-                    <span className='h-12 w-full flex justify-start items-center text-[16px] mb-2 bg-gray-200'><i>Filter by Date</i></span>
-                    <div className='h-15 w-full mb-3 flex-col justify-end' >
-                        <span className='block h-5 w-full text-sm mb-2'><i>From</i></span>
-                        <DatePicker className='block h-8 w-full bg-gray-50 shadow-xl px-2 rounded-sm focus:outline-2 outline-green-600 text-black' variant="borderless" placeholder={getTodaysDate()} onChange={(date, dateString) => setFrom(dateString)} />
-                    </div >
-                    <div className='h-15 w-full flex-col justify-end' >
-                        <span className='block h-5 w-full text-sm mb-2'><i>To</i></span>
-                        <DatePicker className='block h-8 w-full bg-gray-50 shadow-xl px-2 rounded-sm focus:outline-2 outline-green-600 text-black' variant="borderless" placeholder={getTodaysDate()} onChange={(date, dateString) => setTo(dateString)} />
-                    </div >
-                </div>
-                <div className='h-25 w-full mt-3 flex flex-col justify-center items-center'>
-                    <span className='h-12 w-full flex justify-start items-center text-[16px] mb-2 bg-gray-200'><i>Filter by No. of Visitors</i></span>
-                    <input type="number" className='block h-8 w-full bg-gray-50 shadow-xl px-2 rounded-sm  focus:outline-2 outline-emerald-500 text-black' value={noOfVisitors} onChange={handleNoOfVisitorsChange} placeholder='0' />
-                </div>
-                <div className='h-25 w-full'>
-                    <span className='h-12 w-full flex justify-start items-center text-[16px] mb-2 bg-gray-200'><i>Filter By Ammenities</i></span>
-                    <div className='h-8 w-full flex flex-col justify-center itmes-center'>
-                        <select name="filter" id="filter" className='h-full w-full bg-gray-50 shadow-xl' onChange={(e) => setFilterValue(e.currentTarget.value)}>
+            <SelectedFiltersComp />
+            <div className='h-min w-full flex flex-col justify-start items-center'>
+                <FilterTagComp title='Filter by Date'>
+                    <div className='h-33 w-full my-2 flex flex-col justify-start items-center'>
+                        <div className='h-15 w-full mb-3 flex-col justify-end' >
+                            <span className='block h-5 w-full text-sm mb-2'><i>From</i></span>
+                            <DatePicker className='block h-8 w-full bg-gray-50 shadow-xl px-2 rounded-sm focus:outline-2 outline-green-600 text-black' variant="borderless" placeholder={getTodaysDate()} onChange={(date, dateString) => setFrom(dateString)} />
+                        </div>
+                        <div className='h-15 w-full flex-col justify-end' >
+                            <span className='block h-5 w-full text-sm mb-2'><i>To</i></span>
+                            <DatePicker className='block h-8 w-full bg-gray-50 shadow-xl px-2 rounded-sm focus:outline-2 outline-green-600 text-black' variant="borderless" placeholder={getTodaysDate()} onChange={(date, dateString) => setTo(dateString)} />
+                        </div>
+                    </div>
+                </FilterTagComp>
+                <FilterTagComp title='Filter by No of Visitors'>
+                    <input type="number" className='block h-8 my-2 w-full bg-gray-50 shadow-xl px-2  focus:outline-2 outline-emerald-500 text-black' value={noOfVisitors} onChange={handleNoOfVisitorsChange} placeholder='0' />
+                </FilterTagComp>
+                <FilterTagComp title='Filter by Ammenities'>
+                    <div className='h-8 w-full my-2 flex flex-col justify-center itmes-center'>
+                        <select name="filter" id="filter" className='h-full w-full px-2 bg-gray-50 shadow-xl' onChange={(e) => setFilterValue(e.currentTarget.value)}>
                             <option value="Price">Price</option>
                             <option value="LivingRoom">Living Rooms</option>
                             <option value="Bathrooms">Bath Rooms</option>
@@ -65,9 +71,48 @@ const FilterPanel = () => {
                             <option value="Kitchen">Kitchen</option>
                         </select>
                     </div>
-                </div>
+                </FilterTagComp>
+                <FilterTagComp title='Filter by Ratings'>
+                    <input type="number" className='block h-8 my-2 w-full bg-gray-50 shadow-xl px-2  focus:outline-2 outline-emerald-500 text-black' value={noOfVisitors} onChange={handleNoOfVisitorsChange} placeholder='0' />
+                </FilterTagComp>
             </div>
         </div >
+    )
+}
+
+const FilterTagComp = ({ title, children }: FilterPanelUtillityInterface) => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleToggle = () => {
+        setOpen(open => !open);
+    }
+
+    return (
+        <>
+            <div className='h-12 w-full my-2 flex'>
+                <span className='h-full w-48 flex justify-start items-center text-[16px] mb-2' > <i>{title}</i></span >
+                <span className='h-full w-12 flex justify-center items-center' onClick={handleToggle}>
+                    {
+                        !open ?
+                            <Arrow type="filter" mode='down' /> :
+                            <Arrow type="filter" mode='up' />
+                    }
+                </span>
+            </div >
+            <Divider variant='middle' flexItem />
+            {open ? children : null}
+        </>
+    )
+}
+
+const SelectedFiltersComp = () => {
+    return (
+        <div className='h-min-5 w-full my-2 p-1 flex justify-between items-center bg-white rounded-md text-[13px]'>
+            <span className='h-5 w-40'>Filters Applied : None</span>
+            <span className='h-5 w-10 flex justify-center items-center'>
+                <Arrow type="filter" mode='down' dimension='18' />
+            </span>
+        </div>
     )
 }
 
