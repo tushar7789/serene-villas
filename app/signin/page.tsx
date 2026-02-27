87077abb6972ad0dc9fdbf387f683c27cb8abdd4
tushar7789 <tusharjangid7@gmail.com>
2026-02-27 23:23:51 +0530

    moved b.e. authorize errors completely to frontend

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,9 +5,11 @@ import Image from 'next/image';
 import { signIn } from 'next-auth/react';
 
 import { SignInOptionsList } from "../../utils/data/imageDirectory";
+import { useState } from "react";
 
 const LoginPage = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const [error, setError] = useState(false);
 
     const handleSignInOptionClick = (data: { data: any, signInOption: string }) => {// eslint-disable-line @typescript-eslint/no-explicit-any
 
@@ -15,7 +17,21 @@ const LoginPage = () => {
             localStorage.setItem("redirectReason", "signin");
             signIn(data.signInOption);
         } else if (data.signInOption === 'Email') {
-            console.log("using email sign in option --- ", data);
+            try {
+                const res = signIn('credentials', { email: data.data.email, password: data.data.password, redirect: false });
+
+                res.then(d => {
+                    if (d?.error) {
+                        setError(true);
+                        reset();
+                    } else {
+                        setError(false);
+                    }
+                });
+
+            } catch (error) {
+                console.log("error while sign in :", error);
+            }
         }
     }
 
@@ -30,7 +46,9 @@ const LoginPage = () => {
                 <form
                     className='h-56 w-100 rounded-2xl flex flex-col justify-between items-center'
                     onSubmit={handleSubmit((data) => handleSignInOptionClick({ data: data, signInOption: SignInOptionsList[1][0].toString() }))}>
-
+                    <div className="h-5 w-90 text-[13px] text-red-500 ">
+                        {error === true ? `Invalid Email or Password. Try Again!` : ``}
+                    </div>
                     <div className="h-16 w-90 p-0">
                         <span className="h-4 w-full block text-[10px] text-red-500">{errors.email && "Email is required"}</span>
                         <input {...register('email', { required: true })}
@@ -56,7 +74,7 @@ const LoginPage = () => {
 
 const ORBlock = () => {
     return (
-        <div className="h-15 w-90 flex justify-center items-center">
+        <div className="h-10 w-90 flex justify-center items-center border-2">
             OR
         </div>
     )
--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,14 +1,27 @@
-import {NextAuthConfig, AuthError}  from "next-auth";
+import {NextAuthConfig, CredentialsSignin}  from "next-auth";
 import Credentials from "next-auth/providers/credentials"
 import Google from "next-auth/providers/google";
 import Users from "../models/users";
 import argon2 from "argon2"
 import {z} from "zod"
+import dbConnect from "./mongoose";
 
 export const loginSchema = z.object({ 
   email: z.email(), 
   password: z.string().min(6) 
 })
+
+// class InvalidCredential extends CredentialsSignin {
+//   constructor () {
+//     super('Invalid email or password');
+//   }
+// } 
+
+// class InvalidCredentialOrNoUser extends CredentialsSignin {
+//   constructor () {
+//     super('Invalid credentials or User does not exist');
+//   }
+// }
 
 export const authConfig: NextAuthConfig = {
   providers: [Google({
@@ -19,17 +32,15 @@ export const authConfig: NextAuthConfig = {
       credentials: { email: {}, password: {} },
 
       async authorize(credentials) {
+        await dbConnect();
+        
         const validated = loginSchema.safeParse(credentials);
-        if (!validated.success) {
-          throw new AuthError("Invalid email or password");
-        }
+        if (!validated.success) return null;
 
         const { email, password } = validated.data;
 
         const user = await Users.findOne({email : email});
-        if (!user || !user.hashedPassword) {
-          throw new AuthError("Invalid email or password");
-        }
+        if (!user || !user.hashedPassword) return null;
 
         const passwordsMatch = await argon2.verify(user.hashedPassword as any, password); // eslint-disable-line @typescript-eslint/no-explicit-any
 
